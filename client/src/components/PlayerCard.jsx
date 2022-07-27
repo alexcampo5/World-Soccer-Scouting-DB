2edf08b9725cd62cafd62d14aab1f9990c26f7d6
Alex Campo <abrettc@gmail.com>
2022-07-27 13:05:57 -0500

    added in league call to display league name in playercard

--- client/src/components/PlayerCard.jsx
+++ client/src/components/PlayerCard.jsx
@@ -3,16 +3,17 @@ import {useParams} from 'react-router-dom'
 import axios from 'axios'
 import {useNavigate} from 'react-router-dom'
 import EditPlayer from "./EditPlayer"
+import editphoto from '../photos/editimage.png'
 
 export default function PlayerCard (props) {
   const [playerDetails, setPlayerDetails] = useState(null)
   const [showEditForm, setEditForm] = useState(false)
+  const [leagueDetails, setLeagueDetails] = useState(null)
 
   let {id} = useParams()
 
   const getPlayerDetails = async () => {
     let res = await axios.get(`http://localhost:3001/players/${id}`)
-    console.log(res.data.player)
     setPlayerDetails(res.data.player)
   }
 
@@ -25,40 +26,50 @@ export default function PlayerCard (props) {
     console.log('player deleted')
   }
 
+
+  const getLeagueDetails = async () => {
+    let res = await axios.get(`http://localhost:3001/leagues/${playerDetails.league[0]}`)
+    setLeagueDetails(res.data.league)
+  }
+
+  useEffect(() => {
+    getLeagueDetails()
+  }, [playerDetails])
+
   const toggleEditForm = () => {
     setEditForm(!showEditForm)
     console.log(showEditForm)
   }
 
 
-  return playerDetails ? (
+  return playerDetails && leagueDetails ? (
     <div>
       <div className='player-content-container'>
       <h1>{playerDetails.playerName}</h1>
       <img src={playerDetails.image} alt='Player'/>
       <div className="raw-data-container">
         <div className='stat-container'>
-          <h4>Age: {playerDetails.age}</h4> <button onClick={toggleEditForm} className='delete-button'>Edit</button> <EditPlayer category='age' title='Age' visible={showEditForm} currentValue={props.age}/>
+          <h4 className='stat'>Age: {playerDetails.age}</h4> <img onClick={toggleEditForm} className='edit-button' src={editphoto}/> <EditPlayer category='age' title='Age' visible={showEditForm} currentValue={props.age}/>
         </div>
         <div className='stat-container' >
-          <h4>Nationality: {playerDetails.nationality}</h4> <button className='delete-button' onClick={toggleEditForm}>Edit</button> <EditPlayer category='nationality' title='Nationality' visible={showEditForm} currentValue={props.nationality}/>
+          <h4 className='stat'>Nationality: {playerDetails.nationality}</h4> <img onClick={toggleEditForm} className='edit-button' src={editphoto}/> <EditPlayer category='nationality' title='Nationality' visible={showEditForm} currentValue={props.nationality}/>
         </div>
         <div className='stat-container' >
-        <h4>Height: {playerDetails.height}</h4> <button className='delete-button' onClick={toggleEditForm}>Edit</button> <EditPlayer category='height' title='Height' visible={showEditForm} currentValue={props.height}/>
+        <h4 className='stat'>Height: {playerDetails.height}</h4> <img onClick={toggleEditForm} className='edit-button' src={editphoto}/> <EditPlayer category='height' title='Height' visible={showEditForm} currentValue={props.height}/>
         </div>
         <div className='stat-container' >
-        <h4>League: {playerDetails.league[0].leagueName}</h4> <button className='delete-button' onClick={toggleEditForm}>Edit</button> <EditPlayer category='league' title='League' visible={showEditForm} currentValue={props.league}/>
+        <h4 className='stat'>League: {leagueDetails.leagueName}</h4> <img onClick={toggleEditForm} className='edit-button' src={editphoto}/> <EditPlayer category='league' title='League' visible={showEditForm} currentValue={props.league}/>
         </div>
         <div className='stat-container' >
-        <h4>Club: {playerDetails.club}</h4> <button className='delete-button' onClick={toggleEditForm}>Edit</button> <EditPlayer category='club' title='Club' visible={showEditForm} currentValue={props.club}/>
+        <h4 className='stat'>Club: {playerDetails.club}</h4> <img onClick={toggleEditForm} className='edit-button' src={editphoto}/> <EditPlayer category='club' title='Club' visible={showEditForm} currentValue={props.club}/>
         </div>
         <div className='stat-container' >
-        <h4>Skills: {playerDetails.skills}</h4> <button className='delete-button' onClick={toggleEditForm}>Edit</button> <EditPlayer category='skills' title='Skills' visible={showEditForm} currentValue={props.skills}/>
+        <h4 className='stat'>Skills: {playerDetails.skills}</h4> <img onClick={toggleEditForm} className='edit-button' src={editphoto}/> <EditPlayer category='skills' title='Skills' visible={showEditForm} currentValue={props.skills}/>
         </div>
       </div>
       </div>
       <div>
-        <button onClick={deletePlayer}>Delete player</button>
+        <button onClick={deletePlayer} className='delete-button'>Delete player</button>
       </div>
     </div>
   ) : <h1>Player does not exist</h1>;
